8488fd0869a25f9cd544c5c4956a85b55a1636b2
lbouger <lbouger@gmail.com>
2013-06-21 20:56:31 +0300

    Update app.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,26 +1,34 @@
 var formApp = angular.module('formApp', []);
 
 formApp.controller('formController', function($scope, $http){
-
-    $http.get('server/api/users')
+    $scope.unitPrice = 55.65;
+    /*$http.get('server/api/users')
         .success(function(data){
             $scope.users = data.users;
-        });
+        });*/
+        
+    $scope.users = [{fullname:'User1', email:'user1@corp.com', arrival:new Date(), night:5,total:$scope.unitPrice*5},
+    {fullname:'User2', email:'user2@corp.com', arrival:new Date(), night:4,total:$scope.unitPrice*4},
+	{fullname:'User3', email:'user3@corp.com', arrival:new Date(), night:5,total:$scope.unitPrice*5},
+	{fullname:'User4', email:'user4@corp.com', arrival:new Date(), night:3,total:$scope.unitPrice*3},
+	{fullname:'User5', email:'user5@corp.com', arrival:new Date(), night:1,total:$scope.unitPrice*1},
+	{fullname:'User6', email:'user6@corp.com', arrival:new Date(), night:2,total:$scope.unitPrice*2}];
 
-    $scope.confirm = function(user){
-
-        $http.post('server/api/users', user)
+    $scope.confirm = function(user){){
+    	$scope.users.push(user);
+		$scope.user = {};
+        /*ttp.post('server/api/users', user)
             .success(function(data){
                 $scope.users.push(user);
                 // only available from angularjs 1.1.1
                 // https://github.com/angular/angular.js/pull/1127
                 $scope.myForm.$setPristine();
                 $scope.user = {};
-            });
+            });*/
     }
 
     $scope.calculate = function(value){
-        $scope.user.total = 55.65 * value;
+        $scope.user.total = $scope.unitPrice * value;
     }
 
     $scope.checkEmail = function(){
